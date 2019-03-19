571161162fc09360c183d9954e1791b16008a6e7
p_yamauchi <webad.yamauchi@gmail.com>
2019-03-19 16:47:23 +0900

    受け取ってください！

--- js/my.js
+++ js/my.js
@@ -1,13 +1,6 @@
 const hey = 'HEY!';
 
 
-for (let i = 0; i <= 5; i++) {
-	let heyhey = + hey;
-	arrowFunc(heyhey);
-}
+console.log(hey);
 
-let arrowFunc = (v) => {
-	console.log(v);
-}
 
-console.log('なんで動かないんでしょう');
